6637b4e80b000fea9e9a7757e36ea0f09cd3787d
Calvin Morrison <mutantturkey@gmail.com>
2014-02-25 14:08:49 -0500

    take arguments from buildFilter.py optionally, or still prompt if not, use more standard names rather than foo, add me to contributor list, added shebang

--- buildFilter.py
+++ buildFilter.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python
 ###############################################################################
 # Program: buildFilter.py                                                     #
 # Rev: 1.0                                                                    #
 # Date: 1/25/12                                                               #
-# Author: S. Essinger                                #
+# Author: S. Essinger                                                         #
+# Contributor: Calvin Morrison                                                #
 #                                                                             #
-# Description: This script generates the ARB .ift filter for your custom      #        
+# Description: This script generates the ARB .ift filter for your custom      #
 # database. You must provide an input file containing the names of each       #
 # meta-data field residing in your database, one name per line                #
-# (see example: metaLables.txt). The output .ift file should then be placed  #
+# (see example: metaLables.txt). The output .ift file should then be placed   #
 # in the ARB directory under: /arb/lib/import/                                #
 ###############################################################################
+
+import sys
+import argparse
 
 # Function: Obtain number of meta-data labels from input file
 def fileLen(fname):
@@ -18,62 +23,80 @@ def fileLen(fname):
             pass
     return i + 1
 
-inFile = raw_input('Enter name of file containing meta-labels: ')
-#inFile = 'metaLabels.txt' # Name of input file
-noLines = fileLen(inFile) # Get number of meta-labels from input file
-metaLabels = open(inFile,'r') # Open input file for meta-label extraction
-#outFile = open('custom_import_filter.ift','w') # Open output file for writing
-foo = raw_input('Enter desired name of ARB input filter: ')
-outFile = open(foo,'w')
+def main():
+  inFileName = ""
+  noLines = 0
+  metaLabels = ""
+  outFileName = ""
 
-# Include required ARB code to initialize filter.
-outFile.write('AUTODETECT      ">*"'+'\n\n')
-outFile.write('#Global settings:'+'\n')
-outFile.write('KEYWIDTH        1' + '\n\n')
-outFile.write('BEGIN           ">??*"'+'\n\n')
-outFile.write('MATCH		">*"'+'\n')
-outFile.write(r'	    SRT "* *=*1:*\t*=*1"'+'\n')
-outFile.write('	    WRITE "name"'+'\n\n')
-outFile.write('MATCH		">*"'+'\n')
-outFile.write(r'	    SRT "* *=*1:*\t*=*1"'+'\n')
-outFile.write('	    WRITE "acc"'+'\n\n')
+  if len(sys.argv) != 1:
+    parser = argparse.ArgumentParser(description="arb IFT generator for custom databases\n")
+    parser.add_argument("-i", "--input", help="input metalabel file", required=True)
+    parser.add_argument("-o", "--output", help="output ift file", required=True)
+    args = parser.parse_args()
+
+    inFileName = args.input
+    outFileName = args.output
+  else:
+    inFileName = raw_input('Enter name of file containing meta-labels: ')
+    outFileName= raw_input('Enter desired name of ARB input filter: ')
+
+  noLines = fileLen(inFileName) # Get number of meta-labels from input file
+  metaLabels = open(inFileName,'r') # Open input file for meta-label extraction
+  outFile = open(outFileName,'w')
+
+  # Include required ARB code to initialize filter.
+  outFile.write('AUTODETECT      ">*"'+'\n\n')
+  outFile.write('#Global settings:'+'\n')
+  outFile.write('KEYWIDTH        1' + '\n\n')
+  outFile.write('BEGIN           ">??*"'+'\n\n')
+  outFile.write('MATCH		">*"'+'\n')
+  outFile.write(r'	    SRT "* *=*1:*\t*=*1"'+'\n')
+  outFile.write('	    WRITE "name"'+'\n\n')
+  outFile.write('MATCH		">*"'+'\n')
+  outFile.write(r'	    SRT "* *=*1:*\t*=*1"'+'\n')
+  outFile.write('	    WRITE "acc"'+'\n\n')
 
 # Create ARB code to identify, extract and assign meta-data from custom database and
 # to corresponding meta-label.
-count = 1
-SRC = []
-syntax = r'"*\t*'
-syntax1 = '"* *'
-lastPart = r':*\t*=*1"'
-for i in range(0,noLines):
-        if i == 0:
-            SRC.append(syntax1 + '=*' + str(count) + lastPart)
-        else:
-            SRC.append(syntax + '=*' + str(count) + lastPart)
+  count = 1
+  SRC = []
+  syntax = r'"*\t*'
+  syntax1 = '"* *'
+  lastPart = r':*\t*=*1"'
+  for i in range(0,noLines):
+    if i == 0:
+      SRC.append(syntax1 + '=*' + str(count) + lastPart)
+    else:
+      SRC.append(syntax + '=*' + str(count) + lastPart)
 
-        label = metaLabels.readline()
-        outFile.write('MATCH' + '\t' + '\t' + r'">*"' + '\n')
-        outFile.write('\t' + '\t' + 'SRT ' + SRC[i] + '\n')
-        outFile.write('\t' + '\t' + 'WRITE "' + str(label.strip()) + '"\n\n')
-        
-        if count == 9:
-                syntax = syntax + r'=*9:*'
-                count = 1
-        if i >= 1:
-                syntax = (syntax + r'\t*')
+    label = metaLabels.readline()
+    outFile.write('MATCH' + '\t' + '\t' + r'">*"' + '\n')
+    outFile.write('\t' + '\t' + 'SRT ' + SRC[i] + '\n')
+    outFile.write('\t' + '\t' + 'WRITE "' + str(label.strip()) + '"\n\n')
+          
+    if count == 9:
+      syntax = syntax + r'=*9:*'
+      count = 1
+    if i >= 1:
+      syntax = (syntax + r'\t*')
+    count = count + 1
 
-        count = count + 1
+
+  # Append ARB code to .ift filter for the purposes of extracting the sequences
+  # from your custom databse.
+  outFile.write('\n' + 'SEQUENCEAFTER   "*"'+'\n')
+  outFile.write('SEQUENCESRT     ""'+'\n')
+  outFile.write('SEQUENCEACI     "remove(".-0123456789 /")"'+'\n')
+  outFile.write('SEQUENCECOLUMN 	0'+'\n')
+  outFile.write('SEQUENCEEND     ">*"'+'\n\n')
+  outFile.write('DONT_GEN_NAMES'+'\n\n')
+  outFile.write('END             "//"')
+
+  metaLabels.close()
+  outFile.close()
+
+if __name__ == "__main__":
+  sys.exit(main())
 
 
-# Append ARB code to .ift filter for the purposes of extracting the sequences
-# from your custom databse.
-outFile.write('\n' + 'SEQUENCEAFTER   "*"'+'\n')
-outFile.write('SEQUENCESRT     ""'+'\n')
-outFile.write('SEQUENCEACI     "remove(".-0123456789 /")"'+'\n')
-outFile.write('SEQUENCECOLUMN 	0'+'\n')
-outFile.write('SEQUENCEEND     ">*"'+'\n\n')
-outFile.write('DONT_GEN_NAMES'+'\n\n')
-outFile.write('END             "//"')
-
-metaLabels.close()
-outFile.close()
