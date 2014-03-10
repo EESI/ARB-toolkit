1c6b2b4c2020d905f382b533100377e2451779bf
Calvin Morrison <mutantturkey@gmail.com>
2014-03-10 14:42:27 -0400

    split getAccession into two scripts, take arguments and clean it up

---
+++ src/addUIDtoFasta.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# conver
+def main():
+
+  if len(sys.argv) != 1:
+    parser = argparse.ArgumentParser(description="generate aligned fasta file's accessions with unique ID's")
+    parser.add_argument("-i", "--input-accession", help="input accession list (generated with getAccession.py)", required=True)
+    parser.add_argument("-f", "--input-fasta", help="input aligned fasta file", required=True)
+    parser.add_argument("-o", "--output-fasta", help="output fasta with UIDs in the file", required=True)
+    args = parser.parse_args()
+
+		inAccFn = args.input_accession
+		inAlnFn = args.input_fasta
+		outAlnFn = args.output_fasta
+  else:
+    inAccFn  = raw_input('Enter input accession file:')
+    inAlnFn  = raw_input('Enter input aligned fasta file:')
+    outAlnFn = raw_input('Enter output fasta file:')
+
+	inACC = open(inAccFn,'r')
+	inALN = open(inAlnFn,'r')
+	outALN = open(outAlnFn,'w')
+
+	UID = []
+	ACC = []
+	for line in inACC:
+		line = line.strip()
+		line = line.partition('\t')
+		hack = line[2].partition('|')
+		UID.append(str(line[0]))
+		ACC.append(str(hack[0]))
+	inACC.close()
+
+	for line in inALN:
+		if line.startswith('>'):
+			outALN.write('\n')
+			temp = line.partition('pdb|')
+			if len(temp[1]) == 0:
+				temp = line.partition('sp|')
+			temp = temp[2].partition('|')
+			temp = temp[0].partition('.')
+			try:
+				key = ACC.index(temp[0])
+				plug = UID[key]
+				outALN.write('>'+str(plug)+'\n')
+			except ValueError:
+				key = 'MISSING_IN_DATABASE'
+				outALN.write('>'+str(key)+'\n')
+		else:
+			outALN.write(line)
+	inALN.close()
+	outALN.close()
--- src/getAccession.py
+++ src/getAccession.py
@@ -8,51 +8,37 @@
 # Description: Maps unique ID from database to each alignment
 ###############################################################################
 
-inFile = open('MFS_metaData.txt','r')
-outFile = open('ListAccessions.txt','w')
+import sys
+import argparse
 
-for line in inFile:
-  if line.startswith('>'):
-    temp = line.split('\t',12)
-    revised = temp[11].partition('_')
-    if revised[1]:
-      revised = revised[0]+'|A'
-    else:
-      revised = revised[0]
-    outFile.write(str(temp[1])+'\t'+str(revised)+'\n')
-inFile.close()
-outFile.close()
+def main():
 
-inACC = open('ListAccessions.txt','r')
-inALN = open('MFS_Align.fasta','r')
-outALN = open('MFS_UID.fasta','w')
+  if len(sys.argv) != 1:
+    parser = argparse.ArgumentParser(description="Maps unique metadata id's to accession list")
+    parser.add_argument("-i", "--input-metadata", help="input metadata file", required=True)
+    parser.add_argument("-o", "--output-accession", help="output accession list file", required=True)
+    args = parser.parse_args()
 
-UID = []
-ACC = []
-for line in inACC:
-  line = line.strip()
-  line = line.partition('\t')
-  hack = line[2].partition('|')
-  UID.append(str(line[0]))
-  ACC.append(str(hack[0]))
-inACC.close()
-
-for line in inALN:
-  if line.startswith('>'):
-    outALN.write('\n')
-    temp = line.partition('pdb|')
-    if len(temp[1]) == 0:
-      temp = line.partition('sp|')
-    temp = temp[2].partition('|')
-    temp = temp[0].partition('.')
-    try:
-      key = ACC.index(temp[0])
-      plug = UID[key]
-      outALN.write('>'+str(plug)+'\n')
-    except ValueError:
-      key = 'MISSING_IN_DATABASE'
-      outALN.write('>'+str(key)+'\n')
+		inFile = args.input_metadata
+		outFile = args.output_accession
   else:
-    outALN.write(line)
-inALN.close()
-outALN.close()
+    inFile = raw_input('Enter input metadata file:')
+    outFile   = raw_input('Enter output accession list file:')
+
+	inFile = open(inFile,'r')
+	outFile = open(outFile,'w')
+
+	for line in inFile:
+		if line.startswith('>'):
+			temp = line.split('\t',12)
+			revised = temp[11].partition('_')
+			if revised[1]:
+				revised = revised[0]+'|A'
+			else:
+				revised = revised[0]
+			outFile.write(str(temp[1])+'\t'+str(revised)+'\n')
+	inFile.close()
+	outFile.close()
+
+if __name__ == "__main__":
+  sys.exit(main())
