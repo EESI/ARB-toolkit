e3bba70882c94f5ddb3e3b333e3d17a69e3bd122
Calvin Morrison <mutantturkey@gmail.com>
2014-04-02 17:58:32 -0400

    fix tree

--- src/arb_import_pipeline.sh
+++ src/arb_import_pipeline.sh
@@ -42,8 +42,13 @@ treelabels_mapped="$output_dir/mapped_labels"
 ift_filter="$output_dir/$(basename $align_fa).ift"
 
 
+echo "getting accession numbers"
 getAccession.py -i $metadata -o $accession
+echo "adding Unique ID's to aligned fasta"
 addUIDtoFasta.py -i $accession -a $align_fa -o $UID_fa
+echo "extracting leaf names"
 extract_leaf_names.py -i $tree -o $treelabels_original
+echo "renaming tree leaf names"
 rename_tree_leaves.py -a $align_fa -u $UID_fa -l $treelabels_original -o $treelabels_mapped
+echo "building ARB ift filter"
 build_ift_from_metalabels.py -i $metalabels -o $ift_filter
