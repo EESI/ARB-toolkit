34827c99e9451007a0abf0d44ee6ca558760823f
Calvin Morrison <mutantturkey@gmail.com>
2014-04-04 19:51:52 -0400

    more verbose test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 check () { 
 	if [[ "$1" -ne "0" ]]; then
-		echo -n "FAILED "
-    # less <<< $2
+		echo "FAILED $3"
+    if [[ $debug ]]; then 
+			less <<< $2
+			exit 1
+		else 
+			exit 1 
+		fi
 	else
-		echo -n "SUCCESS "
+		echo "SUCCESS $3"
 	fi
 }
 
 
 out="test"
-mkdir  $out
 
 echo "RUNNING PIPELINE"
 ../src/arb_import_pipeline.sh ../example/MFS_Align.fasta ../example/MFS.tree ../example/MFS_metaData.txt ../example/MFS_Field_labels.txt $out 
+if [[ "$?" -ne "0" ]]; then
+	echo "PIPELINE FAILED"
+fi
 
 
 # tests
 res=$(diff ../example/ListAccessions.txt $out/accession)
-check $? $res
-echo "checking accession number "
+check $? "$res" "checking accession number "
 
 res=$(diff ../example/MFS_UID.fasta $out/MFS_Align.fasta)
-check $? $res
-echo -n "checking UID fasta file "
+check $? "$res" "checking UID fasta file "
 
 res=$( diff -w <( sort ../example/TreeLabels_Orig.txt ) <( sort $out/extracted_labels ))
-check $? $res
-echo "checking extracted tree labels "
+check $? "$res" "checking extracted tree labels "
 
 res=$( diff -w <( sort ../example/TreeLabels_Mapped_New.txt ) <( sort $out/mapped_labels ))
-check $? $res
-echo "checking mapped labels "
+check $? "$res" "checking mapped labels "
 
 res=$( diff -w ../example/MFS.tree $out/MFS.tree )
-check $? $res
-echo "checking tree file"
+check $? "$res" "checking tree file"
 
 res=$( diff -w ../example/MFS_import_filter.ift $out/MFS_Align.fasta.ift )
-check $? $res
-echo "checking ARB ift filter"
+check $? "$res" "checking ARB ift filter"
